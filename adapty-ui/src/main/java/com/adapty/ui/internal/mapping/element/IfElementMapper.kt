3c1021ad7b81332d096def0e7356f4307aee8d42
vlad <vlad@vg.codes>
2025-05-16 19:04:03 +0400

    version 3.6.2

--- adapty-ui/src/main/java/com/adapty/ui/internal/mapping/element/IfElementMapper.kt
+++ adapty-ui/src/main/java/com/adapty/ui/internal/mapping/element/IfElementMapper.kt
@@ -21,11 +21,13 @@ internal class IfElementMapper(
         inheritShrink: Int,
         childMapper: ChildMapperShrinkable,
     ): UIElement {
+        val platform = config["platform"]
+        val version = config["version"]
         val key = when {
-            (config["platform"] != null && config["platform"] != "android")
-                    || (config["version"] != null && config["version"] != CONFIGURATION_FORMAT_VERSION) -> "else"
+            (platform != null && platform != "android")
+                    || (version is String && !CONFIGURATION_FORMAT_VERSION.isSameOrNewerVersionThan(version)) -> "else"
             else -> {
-                setOf("then", "else").firstOrNull { key ->
+                listOf("then", "else").firstOrNull { key ->
                     hasVideoSupport || (config[key] as? Map<*, *>)?.get("type") != "video"
                 } ?: "then"
             }
@@ -35,5 +37,30 @@ internal class IfElementMapper(
                 message = "$key in If must not be empty",
                 adaptyErrorCode = AdaptyErrorCode.DECODING_FAILED
             )
+    }
+
+    private fun String.isSameOrNewerVersionThan(older: String): Boolean {
+        fun stringVersionToList(value: String): List<Int> {
+            return value.split('.', '-', ' ')
+                .map { it.toIntOrNull() ?: 0 }
+        }
+        return stringVersionToList(this)
+            .isSameOrNewerVersionThan(stringVersionToList(older))
+    }
+
+    private fun List<Int>.isSameOrNewerVersionThan(older: List<Int>): Boolean {
+        val newer = this.toMutableList()
+        val diffCount = older.size - newer.size
+        if (diffCount > 0) {
+            repeat(diffCount) { newer.add(0) }
+        }
+        for ((index, newerElement) in newer.withIndex()) {
+            val olderElement = older.getOrNull(index) ?: return true
+            when {
+                newerElement > olderElement -> return true
+                newerElement < olderElement -> return false
+            }
+        }
+        return true
     }
 }
